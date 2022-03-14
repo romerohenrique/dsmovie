7b4eafd5c6bf3cbbe909c65cd6e10feb3a858470
NAR <romerohenrique08@gmail.com>
2022-03-13 22:02:03 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -27,10 +27,15 @@ function Listing() {
                 setPage(data);
             });
     }, [pageNumber]);
+
+    const handlePageChange = (newPageNumber: number) => {
+        setPageNumber(newPageNumber);
+    }
+
     return (
         <>
 
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
